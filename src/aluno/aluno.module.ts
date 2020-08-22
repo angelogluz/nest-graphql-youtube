1c9c465de917c7f3a031b1dcc868de2c54c76de2
Natanael da Silva <dasilvanatanael700@gmail.com>
2020-08-22 00:09:01 -0300

    Desenvolve a service e o resolver

--- src/aluno/aluno.module.ts
+++ src/aluno/aluno.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AlunoService } from './aluno.service';
 import { AlunoResolver } from './aluno.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Aluno } from './aluno.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Aluno])
+  ],
   providers: [AlunoService, AlunoResolver]
 })
-export class AlunoModule {}
+export class AlunoModule { }
--- src/aluno/aluno.resolver.ts
+++ src/aluno/aluno.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Args, Mutation, Query } from '@nestjs/graphql';
+import { AlunoService } from './aluno.service';
+import { CreateAlunoInput } from './dto/create-aluno.input';
+import { Aluno } from './aluno.entity';
 
 @Resolver('Aluno')
-export class AlunoResolver {}
+export class AlunoResolver {
+    constructor(
+        private alunoService: AlunoService
+    ) { }
+
+    @Query(() => [Aluno])
+    async Alunos(): Promise<Aluno[]> {
+        const alunos = await this.alunoService.findAllAlunos();
+        return alunos;
+    }
+
+    @Mutation(() => Aluno)
+    async createAluno(
+        @Args('data') data: CreateAlunoInput
+    ): Promise<Aluno> {
+        const aluno = await this.alunoService.createAluno(data);
+        return aluno;
+    }
+}
