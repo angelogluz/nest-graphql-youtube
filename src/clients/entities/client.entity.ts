02d7a53064324af9d2f447c29761f4f9aeb371b2
Eduardo Ribeiro <eduardoxrib@gmail.com>
2020-09-26 10:58:08 -0300

    Criando Fields de Client na Entity

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,7 +1,33 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @ObjectType()
 export class Client {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(() => ID)
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  UpdatedAt!: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  DeletedAt!: Date;
 }
