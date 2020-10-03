f3bf1765b6843b7bc831e6a215592d64ee55e395
arthurggssenac <arthurggs.senac@gmail.com>
2020-10-03 01:14:54 -0300

    Adds encryption to the client service

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -4,6 +4,19 @@ import { CreateClientInput } from './dto/create-client.input';
 import { UpdateClientInput } from './dto/update-client.input';
 import { Repository } from 'typeorm';
 import { Client } from './entities/client.entity';
+import * as NodeRSA from 'node-rsa';
+
+//const key = new NodeRSA({b: 512});
+
+const key = new NodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
+                      'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
+                      'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
+                      'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
+                      'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
+                      'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
+                      'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
+                      'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
+                      '-----END RSA PRIVATE KEY-----');
 
 @Injectable()
 export class ClientsService {
@@ -14,6 +27,11 @@ export class ClientsService {
 
   async findAllClients(): Promise<Client[]> {
     const clients = await this.clientRepository.find();
+    for (let i=0; i < clients.length ; i++) {
+      clients[i].name = key.decrypt(clients[i].name, 'utf8');
+      clients[i].phone = key.decrypt(clients[i].phone, 'utf8');
+      clients[i].email = key.decrypt(clients[i].email, 'utf8');
+    }
     return clients;
   }
 
@@ -22,12 +40,25 @@ export class ClientsService {
     if (!client) {
       throw new NotFoundException('Client not found')
     }
+    
+    client.name = key.decrypt(client.name, 'utf8');
+    client.phone = key.decrypt(client.phone, 'utf8');
+    client.email = key.decrypt(client.email, 'utf8');
     return client;
   }
 
   async createClient(data:CreateClientInput): Promise<Client>{
+    const nameCrypted = key.encrypt(data.name, 'base64');
+    const phoneCrypted = key.encrypt(data.phone, 'base64');
+    const mailCrypted = key.encrypt(data.email, 'base64');
+
+    data.name = nameCrypted;
+    data.phone = phoneCrypted;
+    data.email = mailCrypted;
+
     const client = this.clientRepository.create(data);
-    const clientSaved = await this.clientRepository.save(client);
+    const clientSaved = this.clientRepository.save(client)
+    console.log(clientSaved)
     if (!clientSaved) {
       throw new InternalServerErrorException('Something happened. Try again')
     }
@@ -35,6 +66,19 @@ export class ClientsService {
   }
 
   async updateClient(id: string, data: UpdateClientInput):Promise<Client> {
+    if(data.name){
+      const nameCrypted = key.encrypt(data.name, 'base64');
+      data.name = nameCrypted;
+    }
+    if(data.phone){
+      const phoneCrypted = key.encrypt(data.phone, 'base64');
+      data.phone = phoneCrypted;
+    }
+    if(data.email){
+      const mailCrypted = key.encrypt(data.email, 'base64');
+      data.email = mailCrypted;
+    }
+
     const client = await this.findClientById(id);
     await this.clientRepository.update(client, {...data});
     const clientUpdated = this.clientRepository.create({...client,...data})
