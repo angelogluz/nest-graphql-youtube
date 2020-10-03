66f195a5b8e9a5aac1c5f364dc22660cf3ec8cc6
Eduardo Ribeiro <eduardoxrib@gmail.com>
2020-10-03 10:10:06 -0300

    Fixing client service spec

--- src/clients/clients.service.spec.ts
+++ src/clients/clients.service.spec.ts
@@ -12,11 +12,12 @@ describe('ClientsService', () => {
   let service: ClientsService;
 
   const mockRepository = {
-    findAllClients: jest.fn(),
-    findClientById: jest.fn(),
-    createClient: jest.fn(),
-    updateClient: jest.fn(),
-    deleteClient: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   };
 
   beforeAll(async () => {
@@ -34,11 +35,11 @@ describe('ClientsService', () => {
   });
 
   beforeEach(() => {
-    mockRepository.findAllClients.mockReset();
-    mockRepository.createClient.mockReset();
-    mockRepository.deleteClient.mockReset();
-    mockRepository.findClientById.mockReset();
-    mockRepository.updateClient.mockReset();
+    mockRepository.find.mockReset();
+    mockRepository.create.mockReset();
+    mockRepository.delete.mockReset();
+    mockRepository.findOne.mockReset();
+    mockRepository.update.mockReset();
   });
 
   it('should be defined', () => {
@@ -48,46 +49,46 @@ describe('ClientsService', () => {
   describe('When search All Clients', () => {
     it('should be list all clients', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.findAllClients.mockReturnValue([client, client]);
+      mockRepository.find.mockReturnValue([client, client]);
       const clients = await service.findAllClients();
       expect(clients).toHaveLength(2);
-      expect(mockRepository.findAllClients).toHaveBeenCalledTimes(1);
+      expect(mockRepository.find).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('When search Client By Id', () => {
     it('should find a existing client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.findClientById.mockReturnValue(client);
+      mockRepository.findOne.mockReturnValue(client);
       const clientFound = await service.findClientById('1');
       expect(clientFound).toMatchObject({ name: client.name });
-      expect(mockRepository.findClientById).toHaveBeenCalledTimes(1);
+      expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
     });
     it('should return a exception when does not to find a client', async () => {
-      mockRepository.findClientById.mockReturnValue(null);
+      mockRepository.findOne.mockReturnValue(null);
       expect(service.findClientById('3')).rejects.toBeInstanceOf(
         NotFoundException,
       );
-      expect(mockRepository.findClientById).toHaveBeenCalledTimes(1);
+      expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('When create client', () => {
     it('should create a client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.createClient.mockReturnValue(client);
-      mockRepository.createClient.mockReturnValue(client);
+      mockRepository.save.mockReturnValue(client);
+      mockRepository.create.mockReturnValue(client);
       const savedClient = await service.createClient(client);
 
       expect(savedClient).toMatchObject(client);
-      expect(mockRepository.createClient).toBeCalledTimes(1);
-      // expect(mockRepository.).toBeCalledTimes(1);
+      expect(mockRepository.create).toBeCalledTimes(1);
+      expect(mockRepository.save).toBeCalledTimes(1);
     });
 
     it('should return a exception when doesnt create a client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.createClient.mockReturnValue(null);
-      mockRepository.createClient.mockReturnValue(client);
+      mockRepository.create.mockReturnValue(null);
+      mockRepository.create.mockReturnValue(client);
 
       await service.createClient(client).catch(e => {
         expect(e).toBeInstanceOf(InternalServerErrorException);
@@ -95,8 +96,8 @@ describe('ClientsService', () => {
           message: 'Problem to create a client. Try again',
         });
       });
-      expect(mockRepository.createClient).toBeCalledTimes(1);
-      // expect(mockRepository.save).toBeCalledTimes(1);
+      expect(mockRepository.create).toBeCalledTimes(1);
+      expect(mockRepository.save).toBeCalledTimes(1);
     });
   });
 
@@ -104,12 +105,12 @@ describe('ClientsService', () => {
     it('Should update a client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
       const updatedClient = { name: 'Novo Nome' };
-      mockRepository.findClientById.mockReturnValue(client);
-      mockRepository.updateClient.mockReturnValue({
+      mockRepository.findOne.mockReturnValue(client);
+      mockRepository.update.mockReturnValue({
         ...client,
         ...updatedClient,
       });
-      mockRepository.createClient.mockReturnValue({
+      mockRepository.create.mockReturnValue({
         ...client,
         ...updatedClient,
       });
@@ -120,35 +121,35 @@ describe('ClientsService', () => {
       });
 
       expect(resultClient).toMatchObject(updatedClient);
-      expect(mockRepository.createClient).toBeCalledTimes(1);
-      expect(mockRepository.findClientById).toBeCalledTimes(1);
-      expect(mockRepository.updateClient).toBeCalledTimes(1);
+      expect(mockRepository.create).toBeCalledTimes(1);
+      expect(mockRepository.findOne).toBeCalledTimes(1);
+      expect(mockRepository.update).toBeCalledTimes(1);
     });
   });
 
   describe('When delete Client', () => {
     it('Should delete a existing client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.deleteClient.mockReturnValue(client);
-      mockRepository.findClientById.mockReturnValue(client);
+      mockRepository.delete.mockReturnValue(client);
+      mockRepository.findOne.mockReturnValue(client);
 
       const deletedClient = await service.deleteClient('1');
 
       expect(deletedClient).toBe(true);
-      expect(mockRepository.findClientById).toBeCalledTimes(1);
-      expect(mockRepository.deleteClient).toBeCalledTimes(1);
+      expect(mockRepository.findOne).toBeCalledTimes(1);
+      expect(mockRepository.delete).toBeCalledTimes(1);
     });
 
     it('Should not delete a inexisting client', async () => {
       const client = TestUtilClient.giveAMeAValidClient();
-      mockRepository.deleteClient.mockReturnValue(null);
-      mockRepository.findClientById.mockReturnValue(client);
+      mockRepository.delete.mockReturnValue(null);
+      mockRepository.findOne.mockReturnValue(client);
 
       const deletedClient = await service.deleteClient('9');
 
       expect(deletedClient).toBe(false);
-      expect(mockRepository.findClientById).toBeCalledTimes(1);
-      expect(mockRepository.deleteClient).toBeCalledTimes(1);
+      expect(mockRepository.findOne).toBeCalledTimes(1);
+      expect(mockRepository.delete).toBeCalledTimes(1);
     });
   });
 });
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateClientInput } from './dto/create-client.input';
 import { UpdateClientInput } from './dto/update-client.input';
@@ -9,7 +13,7 @@ import { Client } from './entities/client.entity';
 export class ClientsService {
   constructor(
     @InjectRepository(Client)
-    private clientRepository: Repository<Client>
+    private clientRepository: Repository<Client>,
   ) {}
 
   async findAllClients(): Promise<Client[]> {
@@ -20,24 +24,24 @@ export class ClientsService {
   async findClientById(id: string): Promise<Client> {
     const client = await this.clientRepository.findOne(id);
     if (!client) {
-      throw new NotFoundException('Client not found')
+      throw new NotFoundException('Client not found');
     }
     return client;
   }
 
-  async createClient(data:CreateClientInput): Promise<Client>{
+  async createClient(data: CreateClientInput): Promise<Client> {
     const client = this.clientRepository.create(data);
     const clientSaved = await this.clientRepository.save(client);
     if (!clientSaved) {
-      throw new InternalServerErrorException('Something happened. Try again')
+      throw new InternalServerErrorException('Something happened. Try again');
     }
     return clientSaved;
   }
 
-  async updateClient(id: string, data: UpdateClientInput):Promise<Client> {
+  async updateClient(id: string, data: UpdateClientInput): Promise<Client> {
     const client = await this.findClientById(id);
-    await this.clientRepository.update(client, {...data});
-    const clientUpdated = this.clientRepository.create({...client,...data})
+    await this.clientRepository.update(client, { ...data });
+    const clientUpdated = this.clientRepository.create({ ...client, ...data });
 
     return clientUpdated;
   }
@@ -46,8 +50,8 @@ export class ClientsService {
     const client = await this.findClientById(id);
     const deleted = await this.clientRepository.delete(client);
     if (deleted) {
-      return true
-    } 
+      return true;
+    }
     return false;
   }
 }
