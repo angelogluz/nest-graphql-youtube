6ff07d798f9bb46ad35fba4912b2158c8ef0450c
Pietro da Silva Klug <diheguiparker@gmail.com>
2020-10-10 12:19:24 -0300

    Add Crypt Key to .env

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -12,24 +12,17 @@ import { Client } from './entities/client.entity';
 import * as NodeRSA from 'node-rsa';
 
 
-const key = new NodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
-                      'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
-                      'DFCSoi2lPUW8/zspk0q  WvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
-                      'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
-                      'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
-                      'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
-                      'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
-                      'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
-                      '-----END RSA PRIVATE KEY-----');
+const key = new NodeRSA(process.env.CRYPT_KEY, 'pkcs8-public-pem' );
 
 @Injectable()
 export class ClientsService {
   constructor(
     @InjectRepository(Client)
     private clientRepository: Repository<Client>,
-  ) {}
+  ) {{useSoftDelete: true}}
 
   async findAllClients(): Promise<Client[]> {
+    console.log(process.env.CRYPT_KEY)
     const clients = await this.clientRepository.find();
     for (let i=0; i < clients.length ; i++) {
       clients[i].name = key.decrypt(clients[i].name, 'utf8');
@@ -90,7 +83,7 @@ export class ClientsService {
 
   async deleteClient(id: string): Promise<boolean> {
     const client = await this.findClientById(id);
-    const deleted = await this.clientRepository.softRemove(client);
+    const deleted = await this.clientRepository.softDelete(client);
     if (deleted) {
       return true;
     }
