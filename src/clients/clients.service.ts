7e922981f0d11ff3ffe3bbf32fb99152334b3793
Pietro da Silva Klug <diheguiparker@gmail.com>
2020-10-16 10:32:13 -0300

    Update .env

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -18,12 +18,15 @@ export class ClientsService {
   constructor(
     @InjectRepository(Client)
     private clientRepository: Repository<Client>,
-  ) {{useSoftDelete: true}}
+  ) {
+    {
+      useSoftDelete: true;
+    }
+  }
 
   async findAllClients(): Promise<Client[]> {
-    console.log(process.env.CRYPT_KEY)
     const clients = await this.clientRepository.find();
-    for (let i=0; i < clients.length ; i++) {
+    for (let i = 0; i < clients.length; i++) {
       clients[i].name = key.decrypt(clients[i].name, 'utf8');
       clients[i].phone = key.decrypt(clients[i].phone, 'utf8');
       clients[i].email = key.decrypt(clients[i].email, 'utf8');
@@ -36,14 +39,14 @@ export class ClientsService {
     if (!client) {
       throw new NotFoundException('Client not found');
     }
-    
+
     client.name = key.decrypt(client.name, 'utf8');
     client.phone = key.decrypt(client.phone, 'utf8');
     client.email = key.decrypt(client.email, 'utf8');
     return client;
   }
 
-  async createClient(data:CreateClientInput): Promise<Client>{
+  async createClient(data: CreateClientInput): Promise<Client> {
     const nameCrypted = key.encrypt(data.name, 'base64');
     const phoneCrypted = key.encrypt(data.phone, 'base64');
     const mailCrypted = key.encrypt(data.email, 'base64');
@@ -52,23 +55,23 @@ export class ClientsService {
     data.phone = phoneCrypted;
     data.email = mailCrypted;
     const client = this.clientRepository.create(data);
-    const clientSaved = this.clientRepository.save(client)
+    const clientSaved = this.clientRepository.save(client);
     if (!clientSaved) {
       throw new InternalServerErrorException('Something happened. Try again');
     }
     return clientSaved;
   }
 
-  async updateClient(id: string, data: UpdateClientInput):Promise<Client> {
-    if(data.name){
+  async updateClient(id: string, data: UpdateClientInput): Promise<Client> {
+    if (data.name) {
       const nameCrypted = key.encrypt(data.name, 'base64');
       data.name = nameCrypted;
     }
-    if(data.phone){
+    if (data.phone) {
       const phoneCrypted = key.encrypt(data.phone, 'base64');
       data.phone = phoneCrypted;
     }
-    if(data.email){
+    if (data.email) {
       const mailCrypted = key.encrypt(data.email, 'base64');
       data.email = mailCrypted;
     }
