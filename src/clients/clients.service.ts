5341d3a31373bd40b140fc48a70a1c2600edf89f
Eduardo Ribeiro <eduardoxrib@gmail.com>
2020-10-17 11:11:17 -0300

    Remove node-rsa from client service and fix client create method

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -9,10 +9,6 @@ import { UpdateClientInput } from './dto/update-client.input';
 import { Repository } from 'typeorm';
 import { Client } from './entities/client.entity';
 
-import * as NodeRSA from 'node-rsa';
-
-const key = new NodeRSA(process.env.CRYPT_KEY);
-
 @Injectable()
 export class ClientsService {
   constructor(
@@ -26,11 +22,7 @@ export class ClientsService {
 
   async findAllClients(): Promise<Client[]> {
     const clients = await this.clientRepository.find();
-    for (let i = 0; i < clients.length; i++) {
-      clients[i].name = key.decrypt(clients[i].name, 'utf8');
-      clients[i].phone = key.decrypt(clients[i].phone, 'utf8');
-      clients[i].email = key.decrypt(clients[i].email, 'utf8');
-    }
+
     return clients;
   }
 
@@ -40,20 +32,10 @@ export class ClientsService {
       throw new NotFoundException('Client not found');
     }
 
-    client.name = key.decrypt(client.name, 'utf8');
-    client.phone = key.decrypt(client.phone, 'utf8');
-    client.email = key.decrypt(client.email, 'utf8');
     return client;
   }
 
   async createClient(data: CreateClientInput): Promise<Client> {
-    const nameCrypted = key.encrypt(data.name, 'base64');
-    const phoneCrypted = key.encrypt(data.phone, 'base64');
-    const mailCrypted = key.encrypt(data.email, 'base64');
-
-    data.name = nameCrypted;
-    data.phone = phoneCrypted;
-    data.email = mailCrypted;
     const client = this.clientRepository.create(data);
     const clientSaved = this.clientRepository.save(client);
     if (!clientSaved) {
@@ -63,24 +45,12 @@ export class ClientsService {
   }
 
   async updateClient(id: string, data: UpdateClientInput): Promise<Client> {
-    if (data.name) {
-      const nameCrypted = key.encrypt(data.name, 'base64');
-      data.name = nameCrypted;
-    }
-    if (data.phone) {
-      const phoneCrypted = key.encrypt(data.phone, 'base64');
-      data.phone = phoneCrypted;
-    }
-    if (data.email) {
-      const mailCrypted = key.encrypt(data.email, 'base64');
-      data.email = mailCrypted;
-    }
-
     const client = await this.findClientById(id);
     await this.clientRepository.update(client, { ...data });
     const clientUpdated = this.clientRepository.create({ ...client, ...data });
+    const clientSaved = this.clientRepository.save(clientUpdated);
 
-    return clientUpdated;
+    return clientSaved;
   }
 
   async deleteClient(id: string): Promise<boolean> {
